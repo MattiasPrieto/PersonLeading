a9f68948fdce714e35741cda3d4119e9b1ef8a7c
MattiasPrieto <mattias.prieto@ug.uchile.cl>
2023-12-18 18:34:36 -0300

    brcl

--- src/core/joy_base/src/joy_base.py
+++ src/core/joy_base/src/joy_base.py
@@ -3,11 +3,12 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
+from actionlib_msgs.msg import GoalID  # Importa GoalID
 
 class JoyControll:
     def __init__(self):
         self.subscriber = rospy.Subscriber('/joy', Joy, self._callback)
-        self.publisher = rospy.Publisher('/RosAria/cmd_vel', Twist, queue_size=10)
+        self.publisher = rospy.Publisher('/p3dx/cmd_vel', Twist, queue_size=10)
 
     def _callback(self, msg):
         axes = msg.axes
@@ -21,12 +22,20 @@ class JoyControll:
         button_x = buttons[2]
         button_y = buttons[3]
 
+        if button_a == 1:
+            self.cancel_navigation()
+
         output = Twist()
 
         output.linear.x = adelante * 0.3
         output.angular.z = girar * 1
 
         self.publisher.publish(output)
+
+    def cancel_navigation(self):
+        cancel_msg = GoalID(stamp=rospy.Time(secs=0, nsecs=0), id='')
+        cancel_pub = rospy.Publisher('/move_base/cancel', GoalID, queue_size=10)
+        cancel_pub.publish(cancel_msg)
 
 def main():
     rospy.init_node('JoyControll')
